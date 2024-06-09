c46b0ff231ae379174a710f9fcd33c2a4230103a
Niall Carroll <niallpcarroll@gmail.com>
2024-06-09 21:40:40 +0000

    Fix bug by moving words.js content into main script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,149 @@
 const keyboardDiv = document.querySelector(".keyboard");
 
+// List of questions-hints and answers
+const wordList = [
+    {
+        word: "vivaldi",
+        hint: "Composer known as 'The Red Priest'."
+    },
+    {
+        word: "legato",
+        hint: "Musical direction meaning 'smoothly'."
+    },
+    {
+        word: "quartet",
+        hint: "Group of four players."
+    },
+    {
+        word: "nocturne",
+        hint: "John Field created this musical form."
+    },
+    {
+        word: "fidelio",
+        hint: "Beethoven's only opera."
+    },
+    {
+        word: "haydn",
+        hint: "Composer of the 'Surprise' Symphony."
+    },
+    {
+        word: "dublin",
+        hint: "Composer Charles Villiers Stanford was born in this city."
+    },
+    {
+        word: "baroque",
+        hint: "Musical period associated with J.S. Bach."
+    },
+    {
+        word: "holst",
+        hint: "Composer of 'The Planets' suite."
+    },
+    {
+        word: "slavonic",
+        hint: "Famous dances by Dvořák"
+    },
+    {
+        word: "trout",
+        hint: "Schubert's Piano Quintet in A-Major?"
+    },
+    {
+        word: "giuseppe",
+        hint: "First name of composer Verdi"
+    },
+    {
+        word: "gershwin",
+        hint: "'Rhapsody in Blue' composer."
+    },
+    {
+        word: "india",
+        hint: "Setting for Delibes' 'Lakmé'."
+    },
+    {
+        word: "salieri",
+        hint: "Has was once thought to have poisoned Mozart."
+    },
+    {
+        word: "borodin",
+        hint: "This composer was also a chemistry professor."
+    },
+    {
+        word: "sibelius",
+        hint: "Composer of 'Finlandia'."
+    },
+    {
+        word: "farewell",
+        hint: "Haydn symphony where the performers leave one by one."
+    },
+    {
+        word: "funeral",
+        hint: "A dirge is composed for this event."
+    },
+    {
+        word: "chord",
+        hint: "Group of notes played together."
+    },
+    {
+        word: "crotchet",
+        hint: "A quarter note."
+    },
+    {
+        word: "miserere",
+        hint: "Allegri's most famous choral work."
+    },
+    {
+        word: "aquarium",
+        hint: "Seventh movement of 'Carnival of the Animals'."
+    },
+    {
+        word: "oratorio",
+        hint: "'Messiah' by Handel is one example."
+    },
+    {
+        word: "quickly",
+        hint: "How 'allegro' should be played."
+    },
+    {
+        word: "domingo",
+        hint: "One of the 'Three Tenors'."
+    },
+    {
+        word: "hector",
+        hint: "Berlioz's first name."
+    },
+    {
+        word: "norway",
+        hint: "Grieg was born in this country."
+    },
+    {
+        word: "albeniz",
+        hint: "Nineteenth century Spanish composer."
+    },
+    {
+        word: "giacomo",
+        hint: "Puccini's first name."
+    },
+    {
+        word: "chopin",
+        hint: "He composed the 'Revolutionary' étude."
+    },
+    {
+        word: "emperor",
+        hint: "Beethoven's fifth Piano Concerto?"
+    },
+    {
+        word: "purcell",
+        hint: "Composer of the 'Funeral Music for Queen Mary'."
+    },
+    {
+        word: "baritone",
+        hint: "Vocal range between tenor and bass."
+    },
+    {
+        word: "sparrow",
+        hint: "Mozart Mass nicknamed after this bird."
+    },
+];
+
 // Get random word & question
 const getRandomWord = () => {
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];

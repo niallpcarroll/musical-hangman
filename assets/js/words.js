66d300cbb32faff45460b75a43a193e997b02dc9
Niall Carroll <niallpcarroll@gmail.com>
2024-06-09 20:03:33 +0000

    Replace html keyboard with js keyboard

---
+++ assets/js/words.js
@@ -0,0 +1,123 @@
+// List of questions-hints and answers
+const wordList = [
+    {
+        word: "Vivaldi",
+        hint: "Composer known as 'The Red Priest'."
+    },
+    {
+        word: "Legato",
+        hint: "Musical direction meaning 'smoothly'."
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+    {
+        word: "",
+        hint: ""
+    },
+]

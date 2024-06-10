a0b86f1352f466f82e0f8c095418b884121245a9
Niall Carroll <niallpcarroll@gmail.com>
2024-06-10 21:26:29 +0000

    Add additional questions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,143 +10,155 @@ const playAgain = document.querySelector(".replay-btn");
 // List of questions-hints and answers
 const wordList = [{
         word: "vivaldi",
-        hint: "Composer known as 'The Red Priest'."
+        question: "Composer known as 'The Red Priest'."
     },
     {
         word: "legato",
-        hint: "Musical direction meaning 'smoothly'."
+        question: "Musical direction meaning 'smoothly'."
     },
     {
         word: "quartet",
-        hint: "Group of four players."
+        question: "Group of four players."
     },
     {
         word: "nocturne",
-        hint: "John Field created this musical form."
+        question: "John Field created this musical form."
     },
     {
         word: "fidelio",
-        hint: "Beethoven's only opera."
+        question: "Beethoven's only opera."
     },
     {
         word: "haydn",
-        hint: "Composer of the 'Surprise' Symphony."
+        question: "Composer of the 'Surprise' Symphony."
     },
     {
         word: "dublin",
-        hint: "Composer Charles Villiers Stanford was born in this city."
+        question: "Composer Charles Villiers Stanford was born in this city."
     },
     {
         word: "baroque",
-        hint: "Musical period associated with J.S. Bach."
+        question: "Musical period associated with J.S. Bach."
     },
     {
         word: "holst",
-        hint: "Composer of 'The Planets' suite."
+        question: "Composer of 'The Planets' suite."
     },
     {
         word: "slavonic",
-        hint: "Famous dances by Dvořák"
+        question: "Famous dances by Dvořák"
     },
     {
         word: "trout",
-        hint: "Schubert's Piano Quintet in A-Major?"
+        question: "Schubert's Piano Quintet in A-Major?"
     },
     {
         word: "giuseppe",
-        hint: "First name of composer Verdi"
+        question: "First name of composer Verdi"
     },
     {
         word: "gershwin",
-        hint: "'Rhapsody in Blue' composer."
+        question: "'Rhapsody in Blue' composer."
     },
     {
         word: "india",
-        hint: "Setting for Delibes' 'Lakmé'."
+        question: "Setting for Delibes' 'Lakmé'."
     },
     {
         word: "salieri",
-        hint: "Has was once thought to have poisoned Mozart."
+        question: "Has was once thought to have poisoned Mozart."
     },
     {
         word: "borodin",
-        hint: "This composer was also a chemistry professor."
+        question: "This composer was also a chemistry professor."
     },
     {
         word: "sibelius",
-        hint: "Composer of 'Finlandia'."
+        question: "Composer of 'Finlandia'."
     },
     {
         word: "farewell",
-        hint: "Haydn symphony where the performers leave one by one."
+        question: "Haydn symphony where the performers leave one by one."
     },
     {
         word: "funeral",
-        hint: "A dirge is composed for this event."
+        question: "A dirge is composed for this event."
     },
     {
         word: "chord",
-        hint: "Group of notes played together."
+        question: "Group of notes played together."
     },
     {
         word: "crotchet",
-        hint: "A quarter note."
+        question: "A quarter note."
     },
     {
         word: "miserere",
-        hint: "Allegri's most famous choral work."
+        question: "Allegri's most famous choral work."
     },
     {
         word: "aquarium",
-        hint: "Seventh movement of 'Carnival of the Animals'."
+        question: "Seventh movement of 'Carnival of the Animals'."
     },
     {
         word: "oratorio",
-        hint: "'Messiah' by Handel is one example."
+        question: "'Messiah' by Handel is one example."
     },
     {
         word: "quickly",
-        hint: "How 'allegro' should be played."
+        question: "How 'allegro' should be played."
     },
     {
         word: "domingo",
-        hint: "One of the 'Three Tenors'."
+        question: "One of the 'Three Tenors'."
     },
     {
         word: "hector",
-        hint: "Berlioz's first name."
+        question: "Berlioz's first name."
     },
     {
         word: "norway",
-        hint: "Grieg was born in this country."
+        question: "Grieg was born in this country."
     },
     {
         word: "albeniz",
-        hint: "Nineteenth century Spanish composer."
+        question: "Nineteenth century Spanish composer."
     },
     {
         word: "giacomo",
-        hint: "Puccini's first name."
+        question: "Puccini's first name."
     },
     {
         word: "chopin",
-        hint: "He composed the 'Revolutionary' étude."
+        question: "He composed the 'Revolutionary' étude."
     },
     {
         word: "emperor",
-        hint: "Beethoven's fifth Piano Concerto?"
+        question: "Beethoven's fifth Piano Concerto?"
     },
     {
         word: "purcell",
-        hint: "Composer of the 'Funeral Music for Queen Mary'."
+        question: "Composer of the 'Funeral Music for Queen Mary'."
     },
     {
         word: "baritone",
-        hint: "Vocal range between tenor and bass."
+        question: "Vocal range between tenor and bass."
     },
     {
         word: "sparrow",
-        hint: "Mozart Mass nicknamed after this bird."
+        question: "Mozart Mass nicknamed after this bird."
+    },
+    {
+        word: "czerny",
+        question: "Author of famous piano exercises."
+    },
+    {
+        word: "scherzo",
+        question: "A playful composition?"
+    },
+    {
+        word: "sergei",
+        question: "Rachmaninoff's first name."
     },
 ];
 
@@ -170,11 +182,11 @@ const reset = () => {
 const getRandomWord = () => {
     const {
         word,
-        hint
+        question
     } = wordList[Math.floor(Math.random() * wordList.length)];
     newWord = word;
     console.log(word);
-    document.querySelector(".question-text").innerText = hint;
+    document.querySelector(".question-text").innerText = question;
     reset();
 }
 
